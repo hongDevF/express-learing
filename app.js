8fa34ef4b7981533f2a7c3104b6e712f6109a026
senghong <senghong@gmail.com>
2023-04-23 02:34:18 +0700

    fetch product

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Hello new App");
-});
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.listen(3000, ()=>{
-    console.log("http://localhost:3000");
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
