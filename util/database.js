f0f44de2208995b453e3eecc1dad88b568b9c398
senghong <senghong@gmail.com>
2023-04-23 02:44:28 +0700

    u

--- util/database.js
+++ util/database.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'nodecomplete'
+  host: "localhost",
+  user: "root",
+  database: "express",
+  password: "senghong",
+  insecureAuth : true
 });
 
 module.exports = pool.promise();
